d9f6b460dfda550027f3d9ec7586338977bf4e3b
a.mansy <ahmedmansy265@gmail.com>
2025-05-13 22:38:25 +0300

    rename doctype neighborhood to district ,translate options of fields company activity and car type ,adding permissions ,adding filters

--- tst/tst/doctype/district/district.py
+++ tst/tst/doctype/district/district.py
@@ -6,15 +6,14 @@ from frappe.model.document import Document
 
 
 class District(Document):
-	def validate(self):
-		self.rename_document()
+    def validate(self):
+        self.rename_document()
 
+    def rename_document(self):
+        if not self.is_new():
+            # Generate the new name
+            new_name = f"{self.english_name} - {self.arabic_name}"
 
-	def rename_document(self):
-		if not self.is_new():
-			# Generate the new name
-			new_name = f"{self.english_name} - {self.arabic_name}"
-			
-			# Rename the document
-			if self.name != new_name:
-				frappe.rename_doc('District', self.name, new_name)
+            # Rename the document
+            if self.name != new_name:
+                frappe.rename_doc("District", self.name, new_name)
