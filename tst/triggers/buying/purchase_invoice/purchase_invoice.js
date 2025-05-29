81c5b474330e565251b56d4c7339a477bded1dae
Ahmed Emam <ahmedemamhatem@gmail.com>
2025-05-29 23:33:45 +0000

    fix purchase

--- tst/triggers/buying/purchase_invoice/purchase_invoice.js
+++ tst/triggers/buying/purchase_invoice/purchase_invoice.js
@@ -1,53 +1,87 @@
 frappe.ui.form.on('Purchase Invoice', {
     refresh: function(frm) {
+        // Only show the dialog if needed
         if (frm.doc.docstatus === 1){
             if (frm.doc.custom_landed_cost === 1) {
                 frm.add_custom_button(
                     __('Create Landed Cost Voucher'),
                     function() {
                         frm.events.create_landed_cost_voucher(frm);
+                    }
+                );
+            } else if (frm.doc.custom_landed_cost === 0) {
+                // Define a function to show the dialog, so it can be called recursively
+                function show_yes_only_dialog() {
+                    let action_taken = false;
+                    let d = new frappe.ui.Dialog({
+                        title: __("Confirmation"),
+                        fields: [
+                            {
+                                fieldtype: "HTML",
+                                options: '<div style="margin: 20px 0; font-size: 1.1em; text-align: center;">'
+                                    + __("Create Landed Cost Voucher for this Purchase Invoice?")
+                                    + '</div>'
+                            }
+                        ],
+                        primary_action_label: __("Yes"),
+                        primary_action: function() {
+                            action_taken = true;
+                            d.hide();
+                            frappe.call({
+                                method: 'frappe.client.set_value',
+                                args: {
+                                    doctype: 'Purchase Invoice',
+                                    name: frm.doc.name,
+                                    fieldname: 'custom_landed_cost',
+                                    value: 1
+                                },
+                                callback: function(r) {
+                                    if (!r.exc) {
+                                        frm.reload_doc();
+                                        frappe.show_alert({message: __('Updated'), indicator:'green'});
+                                    }
+                                }
+                            });
+                        }
                     });
-            }else if (frm.doc.custom_landed_cost === 0) {
-                frappe.confirm(
-                                    __(
-                                        "Create Landed Cost Voucher for this Purchase Invoice?"
-                                    ),
-                                    () => {
+
+                    // Remove the close (X) button
+                    d.$wrapper.find('.modal-header .close').remove();
+
+                    // Prevent closing by ESC or clicking outside
+                    var $modal = d.$wrapper.closest('.modal');
+                    $modal.attr('data-backdrop', 'static');
+                    $modal.attr('data-keyboard', 'false');
+                    $modal.modal({backdrop: 'static', keyboard: false});
+
+                    // If dialog is closed by any means except "Yes", re-show it
+                    $modal.on('hide.bs.modal', function(e) {
+                        setTimeout(function() {
+                            if (!action_taken) show_yes_only_dialog();
+                        }, 200);
+                    });
+
+                    d.show();
+                }
+
+                // Show dialog for the first time
+                show_yes_only_dialog();
+            }
+        }
+    },
+    create_landed_cost_voucher: function(frm) {
         frappe.call({
-            method: 'frappe.client.set_value',
+            method: "erpnext.stock.doctype.purchase_receipt.purchase_receipt.make_lcv",
             args: {
-                doctype: 'Purchase Invoice',
-                name: frm.doc.name,
-                fieldname: 'custom_landed_cost',
-                value: 1
+                doctype: frm.doc.doctype,
+                docname: frm.doc.name,
             },
-            callback: function(r) {
-                if (!r.exc) {
-                    frm.reload_doc();
-                    frappe.show_alert({message: __('Updated'), indicator:'green'});
+            callback: (r) => {
+                if (r.message) {
+                    var doc = frappe.model.sync(r.message);
+                    frappe.set_route("Form", doc[0].doctype, doc[0].name);
                 }
             }
         });
     }
-);
-            };
-        }
-            
-        },
-    create_landed_cost_voucher: function(frm) {
-		frappe.call({
-			method: "erpnext.stock.doctype.purchase_receipt.purchase_receipt.make_lcv",
-			args: {
-				doctype: frm.doc.doctype,
-				docname: frm.doc.name,
-			},
-			callback: (r) => {
-				if (r.message) {
-					var doc = frappe.model.sync(r.message);
-					frappe.set_route("Form", doc[0].doctype, doc[0].name);
-				}
-			},
-		});
-	
-    },
 });
--- tst/triggers/buying/purchase_order/purchase_order.py
+++ tst/triggers/buying/purchase_order/purchase_order.py
@@ -6,15 +6,30 @@ def validate(doc, method):
     """
     Validate the Purchase Order document.
     """
-    if doc.grand_total <= 1000:
-        doc.custom_po_types = "مشتريات استهلاكية"
-    elif doc.grand_total > 1000 and (doc.custom_po_types == "مشتريات استهلاكية" or not doc.custom_po_types):
+    # Value check for استهلاكية
+    if doc.grand_total > 1000 and (doc.custom_po_types == "مشتريات استهلاكية" or not doc.custom_po_types):
         frappe.throw(_("Purchase Value must be less than 1000 to be considered as استهلاكية and have to create quotation\
                        or choose another purchase Type" ))
 
-    if doc.custom_po_types in ["مشتريات دولية", "مشتريات محلية"] and not doc.custom_quotation:
-        frappe.throw(_("Please create a quotation for this Purchase Order"))
+    # Check if PO Type mandates supplier quotation
+    if doc.custom_po_types:
+        po_type_doc = frappe.get_doc("PO Types", doc.custom_po_types)  
 
+        if po_type_doc.supplier_quotation_mandatory == 1:
+            for item in doc.items:
+                if not item.supplier_quotation:
+                    frappe.throw(_("Please ensure every item row has a Quotation specified for this Purchase Order."))
+
+    # supplier country and internal PO type check
+    
+    if doc.supplier and doc.custom_po_types:
+        o_type_doc = frappe.get_doc("PO Types", doc.custom_po_types)  
+        supplier_doc = frappe.get_doc("Supplier", doc.supplier)
+        
+        if supplier_doc.country == "Saudi Arabia" and po_type_doc.is_internal == 0:
+            frappe.throw(_("For suppliers in Saudi Arabia, PO Type must be marked as Internal."))
+        if po_type_doc.is_internal == 1 and supplier_doc.country != "Saudi Arabia":
+            frappe.throw(_("For suppliers outside Saudi Arabia, PO Type must be marked as Internal."))
 
 @frappe.whitelist()
 def on_update_after_submit(doc, method):
