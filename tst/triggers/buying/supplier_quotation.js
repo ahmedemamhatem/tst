ee92ce4f5078968803015efed10bd00c1c34488b
a.mansy <ahmedmansy265@gmail.com>
2025-08-27 00:32:48 +0300

    supp fields

--- tst/triggers/buying/supplier_quotation.js
+++ tst/triggers/buying/supplier_quotation.js
@@ -1,17 +1,41 @@
 frappe.ui.form.on("Supplier Quotation", {
-    onload: function(frm) {
-        // frappe.msgprint("tst")
+    before_save: function(frm) {
         if (frm.doc.company) {
-            frappe.db.get_value(
-                "Purchase Taxes and Charges Template",
-                { company: frm.doc.company, disabled: 0, is_default: 1 },
-                "name"
-            ).then(r => {
-                if (r.message && r.message.name) {
-                    frm.set_value("taxes_and_charges", r.message.name);
-                    frm.refresh_field("taxes_and_charges")
+            frappe.db.get_list("Purchase Taxes and Charges Template", {
+                filters: { company: frm.doc.company, disabled: 0, is_default: 1 },
+                fields: ["name"],
+                limit: 1
+            }).then(r => {
+                if (r && r.length > 0) {
+                    let template_name = r[0].name;
+
+                    // set the template
+                    frm.set_value("taxes_and_charges", template_name);
+
+                    // fetch full doc including child table
+                    frappe.db.get_doc("Purchase Taxes and Charges Template", template_name)
+                        .then(doc => {
+                            if (doc.taxes && doc.taxes.length > 0) {
+                                // clear existing taxes first
+                                frm.clear_table("taxes");
+
+                                // copy rows
+                                doc.taxes.forEach(d => {
+                                    let row = frm.add_child("taxes");
+                                    row.charge_type = d.charge_type;
+                                    row.account_head = d.account_head;
+                                    row.rate = d.rate;
+                                    row.tax_amount = d.tax_amount;
+                                    row.description = d.description;
+                                    row.cost_center = d.cost_center;
+                                    row.base_tax_amount = d.base_tax_amount;
+                                    // add any other fields you need from template row
+                                });
+                                frm.refresh_field("taxes");
+ }
+                        });
                 }
             });
-        }
-    }
-});
+            }
+    }});
+
